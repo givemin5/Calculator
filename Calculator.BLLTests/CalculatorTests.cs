4695f19baa8d96d87d40db1d058b2ad76b5049b8
givemin5 <givemin5@gmail.com>
2016-09-20 10:59:24 +0800

    增加Calculator Add的測試案例(Unit Test)

---
+++ Calculator.BLLTests/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.BLL.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest_First_1_Second_2_Return_3()
+        {
+            //Arrange
+            int first = 1;
+            int second = 2;
+            int expected = 3;
+            Calculator calc = new Calculator();
+
+            //Act
+            int actual = calc.Add(first, second);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
